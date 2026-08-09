6283deda0826be173a2adda7a354d18d9735ee32
whale2200d <whale2200d@gmail.com>
2026-08-09 10:01:05 +0900

    fix(App.css): 사용하지 않는 CSS 파일 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CaseListPage } from '@/pages/CaseListPage'
 import { CaseChatPage } from '@/pages/CaseChatPage'
